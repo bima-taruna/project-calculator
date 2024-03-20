7aee84492b2db87b60285477c2a81a56c811849d
bima-taruna <bimataruna21@gmail.com>
2024-03-20 11:16:23 +0700

    implement add function in script js

--- script/add/add.spec.js
+++
@@ -1,7 +0,0 @@
-const add = require("./add")
-
-describe("add", () => {
-    test("add 2 number", () => {
-        expect(add(1, 2)).toEqual(3)
-    })
-})
--- script/script.js
+++ script/script.js
@@ -1,15 +1,28 @@
 const digits = document.querySelectorAll(".digit")
 const display = document.querySelector(".display")
 const resetButton = document.querySelector(".ac")
-const displayArr = []
+const addButton = document.querySelector(".add")
+const resultButton = document.querySelector(".result")
+let displayArr = []
 let firstNum;
 let secondNum;
+let result;
+
 
 digits.forEach((digit) => {
     digit.addEventListener("click", () => {
         if (displayArr.length < 9) {
-            displayArr.push(digit.textContent)
-            display.textContent = displayArr.join("")
+            if (digit.classList.contains("operations")) {
+                displayArr.push(digit.textContent)
+                display.textContent = displayArr.join("")
+                secondNum = parseInt(displayArr.join(""))
+                console.log(secondNum)
+            } else {
+                displayArr.push(digit.textContent)
+                display.textContent = displayArr.join("")
+                firstNum = parseInt(displayArr.join(""))
+                console.log(firstNum)
+            }
         }
     })
 })
@@ -18,5 +31,30 @@ resetButton.addEventListener("click", () => {
     displayArr = []
     display.textContent = "0"
 })
+
+addButton.addEventListener("click", () => {
+    displayArr = []
+    digits.forEach((item) => {
+        item.classList.add("operations")
+    })
+    resultButton.classList.add("add")
+})
+
+resultButton.addEventListener("click", () => {
+    if (resultButton.classList.contains("add")) {
+        result = add(firstNum, secondNum)
+        display.textContent = result
+        displayArr = []
+        firstNum = ""
+        secondNum = ""
+        digits.forEach((digit) => {
+            digit.classList.remove("operations")
+        })
+    }
+})
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
 
 
